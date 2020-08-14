5b52a1b9d64ef5ecb0e458033154dcfcb8ae6f14
mshahmalaki <mshahmalaki@gmail.com>
2020-08-14 15:50:45 +0430

    Intial Commit

---
+++ app.py
@@ -0,0 +1,133 @@
+from os import path
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+basedir = path.abspath(path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + path.join(basedir, 'planet.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database Created')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database Dropped!!!')
+
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(
+        name='Mercury',
+        type='Class D',
+        home_star='Sol',
+        mass=3.258e23,
+        radius=1516,
+        distance=35.98e6
+    )
+    venus = Planet(
+        name='Venus',
+        type='Class K',
+        home_star='Sol',
+        mass=4.867e24,
+        radius=3760,
+        distance=67.24e6
+    )
+    earth = Planet(
+        name='Earth',
+        type='Class M',
+        home_star='Sol',
+        mass=5.972e24,
+        radius=3959,
+        distance=92.96e6
+    )
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(
+        first_name='Wiliam',
+        last_name='Herschel',
+        email='test@test.com',
+        password='P@ssw0rd'
+    )
+
+    db.session.add(test_user)
+
+    db.session.commit()
+    print('Database Seeded!')
+
+
+@app.route('/planets', methods=['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
+
+
+# Database Models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = (
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'password'
+        )
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = (
+            'id',
+            'name',
+            'type',
+            'home_star',
+            'mass',
+            'radius',
+            'distance'
+        )
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
+
+
+if __name__ == "__main__":
+    app.run()
